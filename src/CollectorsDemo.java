40cc1a773c6faa40aafcc679289f9acd93916838
balamurugan-venkadachalam <engan.balamurugan@gmail.com>
2018-04-17 12:02:38 +1200

    Update CollectorsDemo.java

--- src/CollectorsDemo.java
+++ src/CollectorsDemo.java
@@ -13,7 +13,17 @@ public class CollectorsDemo {
 		 List<Person> people = Arrays.asList(
 		     new Person("bala@test.com", "bala", 27),
 		     new Person("raju@test.com", "raju", 30),
-		     new Person("ram@test.com", "ram", 20)
+		     new Person("ram@test.com", "ram", 20),
+                     new Person("test1@test.com","kumar", 30),
+	             new Person("test2@test.com","bala", 200),
+                     new Person("test3@test.com","ram", 40),
+                     new Person("test4@test.com","bala", 50),
+                     new Person("test5@test.com","bala", 60),
+                     new Person("test6@test.com","bala", 70),
+                     new Person("test7@test.com","bala", 80),
+                     new Person("test8@test.com","bala", 90),
+                     new Person("test9@test.com","bala", 43),
+                     new Person("test10@test.com","bala", 46) 
 		 );
 				
 		 Map<String, Person> mapEmailPerson = 
@@ -42,5 +52,23 @@ public class CollectorsDemo {
 		 
 		 System.out.println(result5);
 		
+		        Person result6 = persons.stream()                        // Convert to steam
+                	.filter(x -> "bala".equals(x.getName()))        // we want "bala" only
+                	.findAny()                                      // If 'findAny' then return found
+                	.orElse(null);                                  // If not found, return null
+
+        	System.out.println(result6);
+		
+	//peek - consume a stream without ending the pipeline of operations 
+		Long count = persons.stream().parallel()
+		.peek(t ->System.out.println("before filter "+ t))
+		.filter(x -> "bala".equals(x.getName())) 
+		.peek(t ->System.out.println("after name  filter "+ t))        
+		.filter(x -> x.getAge() >50)
+		.peek(t ->System.out.println("after age filter "+ t))  
+		.limit(10)
+		.count();
+        
+       		 System.out.println(count);
 	}
 }
